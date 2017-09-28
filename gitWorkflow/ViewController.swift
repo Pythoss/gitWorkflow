635d78ab08b4f7ade532dec7862a4f75a55681d8
Brian Riddle <bar6x2@atm0560955dyn.dhcp.missouri.edu>
2017-09-28 13:26:04 -0500

    Initial Commit

---
+++ gitWorkflow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitWorkflow
+//
+//  Created by Brian Riddle on 9/28/17.
+//  Copyright © 2017 Brian Riddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
